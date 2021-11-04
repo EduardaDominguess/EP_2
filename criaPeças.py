3c293c39dd8bd14fde32ed897effd2ea79578e31
HenriqueKrepel <92476696+HenriqueKrepel@users.noreply.github.com>
2021-11-04 14:35:37 -0300

    Update criaPeças.py

--- criaPeças.py
+++ criaPeças.py
@@ -2,14 +2,14 @@ import random
 
 def cria_pecas():
     pecas = []
-    i=0
-    while i<=6:
-        ii=0
-        while ii<=6:
+    i = 0
+    while i <= 6:
+        ii = 0
+        while ii <=6 :
             if [i, ii] not in pecas and [ii, i] not in pecas:
                 pecas.append([i, ii])
-            ii+=1
-        i+=1
+            ii += 1
+        i += 1
     random.shuffle(pecas)
     return pecas
 
