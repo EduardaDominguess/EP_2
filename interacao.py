b77071ac620868a99fa4260205f0712f00765845
EduardaDominguess <91220243+EduardaDominguess@users.noreply.github.com>
2021-11-07 17:20:47 -0300

    Update interacao.py

--- interacao.py
+++ interacao.py
@@ -1,40 +1,75 @@
-from cria_pecas import *
+import numpy as np
+from criaPeças import *
 from inicia_jogo import *
 from Possível_mão import *
 from Soma_de_peças import *
 from verifica_ganhador import *
 from AdicionaMesa import *
-
+from random import *
 
 print()
 print('Jogo de Dominó')
 print()
 
-n = print(input('Você deseja jogar? s/n ')
-if n == n:
-print()
-print('Jogo de Dominó')
-print()
-          
-elif n == s:
 
-
-players = 0
-while players < 2 or players > 4:
-  players = int(input('Informe o número de Jogadores (2-4): '))
-  if players < 2 or players > 4:
-    print('Resposta inválida')
+def num_jogadores():
+  players = 0
+  while players < 2 or players > 4:
+    players = int(input('Informe o número de Jogadores (2-4): '))
+    if players < 2 or players > 4:
+      print('Resposta inválida')
+  return players
 
 
 peças = cria_pecas
 inicio = inicia_jogo
 mesa = inicio ['mesa']
 pecas_jogadores = inicio['jogadores']
+monte = inicio['monte']
 
-ordem = np.arange(0, players)
-random.shuffle(ordem)
+ordem_jogadores = np.arange(0, num_jogadores)
+random.shuffle(ordem_jogadores)
 
 print(f'Temos {num_jogadores} com 7 peças cada. \n')
 
 jogo = True
 while jogo: 
+    for i in ordem_jogadores:
+        def formatacao():
+            print(f'Tamanho monte {len(pecas_jogadores)} peças.')
+            print(f'Mesa: {mesa}')
+            print('')
+
+            if i != 0:
+                print(f'Peças jogador {i}: {len(pecas_jogadores[i])} ')
+            else:
+                print(f'Peças jogador {0}: {len(pecas_jogadores[0])} ')
+                print(pecas_jogadores[0])
+            
+            print('')
+
+            if len(posicoes_possiveis(mesa,pecas_jogadores[i])) >= 1  and i == 0 :
+                print(posicoes_possiveis(mesa, peças_jogadores[i])
+           
+
+            if i == 0:
+                começa_a_jogar = int(input('Escolha uma peça para jogar: '))
+                mesa_add(pecas_jogadores[i][começa_a_jogar], mesa)
+                pecas_jogadores[i].pop(jogar)
+
+#Comprar do monte
+
+            comprar_do_monte = 0
+            while len(posicoes_possiveis(mesa, pecas_jogadores[0])) == 0 and len(pecas_jogadores[0]) > 0:
+                entrada = input('Não tem peças possíveis. PEGANDO DO MONTE! [pressione ENTER]')
+            
+                if entrada == '':
+                    pecas_jogadores[0].append(monte(0))
+                    monte.pop(0)
+                    compra_do_monte += 1
+
+                else: 
+                    print('Opção inválida [pressione ENTER]' )
+
+                      
+
