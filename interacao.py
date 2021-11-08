7a0ac51c0b072149ccfb4db99da138d459320bf3
HenriqueKrepel <92476696+HenriqueKrepel@users.noreply.github.com>
2021-11-07 22:51:05 -0300

    Update interacao.py

--- interacao.py
+++ interacao.py
@@ -4,6 +4,7 @@ from inicia_jogo import *
 from posicoes_possiveis import *
 from Soma_de_peças import *
 from verifica_ganhador import *
+from  import *
 from AdicionaMesa import *
 from random import *
 
